554cfce33e4aab5a75996b1e43cfc0e16d734335
ParkJiyoung <realplay23@naver.com>
2016-07-11 10:36:08 +0900

    [CREATE] Project

---
+++ Miplabinternship/app/src/main/java/kr/ac/cbnu/miplab/miplabinternship/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.cbnu.miplab.miplabinternship;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
