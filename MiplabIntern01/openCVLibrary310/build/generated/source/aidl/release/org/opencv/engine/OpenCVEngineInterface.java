4b8bcaa910de23c0a0247a492a1c4f8075ab22c0
ParkJiyoung <realplay23@naver.com>
2016-07-11 23:23:42 +0900

    [CHANGE] add button(camera, gallery) in activity_main.xml

--- MiplabIntern01/openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ MiplabIntern01/openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/JiyoungPark/Documents/SDK/MiplabIntern01/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/JiyoungPark/Documents/Code/MIPlab/MiplabIntern01/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
