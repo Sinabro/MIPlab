9048251c45d351c23793d45e8cd8c8e47ed2b95f
ParkJiyoung <realplay23@naver.com>
2016-07-30 18:51:56 +0900

    [ADD] Save the image at "MIPLAB" directory.

--- MiplabIntern01/app/src/main/java/kr/ac/cbnu/miplab/miplabintern01/MainActivity.java
+++ MiplabIntern01/app/src/main/java/kr/ac/cbnu/miplab/miplabintern01/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.cbnu.miplab.miplabintern01;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,9 +11,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
         initListener();
     }
 
-    private void initView(){
+    private void initView() {
         imageView = (ImageView) findViewById(R.id.imageView);
         buttonCamera = (Button) findViewById(R.id.btn_camera);
         buttonGallery = (Button) findViewById(R.id.btn_gallery);
     }
 
-    private void initListener(){
+    private void initListener() {
 
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,14 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
 
-                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                
-                startActivityForResult(intent, PICK_FROM_GALLERY);
-
-                /*
-                Intent intent = new Intent().setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-
                 intent.putExtra("crop", "true");
                 intent.putExtra("aspectX", 0);
                 intent.putExtra("aspectY", 0);
@@ -81,53 +81,61 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("outputY", 150);
 
                 try {
-                    intent.putExtra("return-data", true);
-                    startActivityForResult(Intent.createChooser(intent, "Choose an image"), PICK_FROM_GALLERY);
+                    intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, PICK_FROM_GALLERY);
                 } catch (ActivityNotFoundException e) {
                     // Do nothing for now
-                }*/
+                }
             }
         });
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == PICK_FROM_CAMERA) {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap photo = extras.getParcelable("data");
-                imageView.setImageBitmap(photo);
+        Bundle extras = data.getExtras();
+
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            switch (requestCode) {
+                case PICK_FROM_CAMERA:
+                    imageView.setImageBitmap(photo);
+                    break;
+
+                case PICK_FROM_GALLERY:
+                    imageView.setImageBitmap(photo);
+                    break;
             }
+
+            saveImage(photo);
         }
-        if (requestCode == PICK_FROM_GALLERY) {
+    }
 
+    private void saveImage(Bitmap photo) {
+
+        final File saveDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + "MIPLAB" + File.separator);
+
+        String filename = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date(System.currentTimeMillis()));
+
+        File fileCacheItem = new File(saveDir + File.separator + filename + ".jpg");
+        Log.e("FILE NAME : ", saveDir + File.separator + filename + ".jpg");
+        OutputStream out = null;
+
+        if (!saveDir.exists() && !saveDir.isDirectory()) {
+            saveDir.mkdir(); /* Create Directory "MIPMAP" */
+        }
+
+        try {
+            fileCacheItem.createNewFile();
+            out = new FileOutputStream(fileCacheItem);
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             try {
-                //Uri에서 이미지 이름을 얻어온다.
-                //String name_Str = getImageNameToUri(data.getData());
-
-                //이미지 데이터를 비트맵으로 받아온다.
-                Bitmap image_bitmap 	= MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
-                ImageView image = (ImageView)findViewById(R.id.imageView);
-
-                //배치해놓은 ImageView에 set
-                image.setImageBitmap(image_bitmap);
-
-                //Toast.makeText(getBaseContext(), "name_Str : "+name_Str , Toast.LENGTH_SHORT).show();
-
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            catch (FileNotFoundException e) { 		e.printStackTrace(); 			}
-            catch (IOException e)                 {		e.printStackTrace(); 			}
-            catch (Exception e)		         {             e.printStackTrace();			}
-
-
-            /*
-            Bundle extras2 = data.getExtras();
-            Log.e("GALLERY : ", data.getExtras().toString());
-            if (extras2 != null) {
-                Bitmap photo = extras2.getParcelable("data");
-                imageView.setImageBitmap(photo);
-                Log.e("if문 안 : ", "들어왔다");
-            }*/
         }
     }
 }
